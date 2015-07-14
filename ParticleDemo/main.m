74acdf46a6c91056f695d6c51ca52f9265fd05f8
davidrynn <davidrynn@gmail.com>
2015-07-14 09:51:28 -0400

    Initial commit

---
+++ ParticleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParticleDemo
+//
+//  Created by David Rynn on 7/14/15.
+//  Copyright (c) 2015 David Rynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
